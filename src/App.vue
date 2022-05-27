1d3695feff09b4ba6294959904f2121562708fad
unknown <antonenko.a.b@gmail.com>
2022-05-27 16:26:27 +0300

    Сделано ДЗ №1

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class='app'>
+		<div class='wrap'>
+			<Button text='Default'></Button>
+			<Button text='Primary' additionalClass='btn-primary'></Button>
+			<Button text='Success' additionalClass='btn-success'></Button>
+			<Button text='Error' additionalClass='btn-error'></Button>
+			<Button text='Disabled' disabled></Button>
+		</div>
+		<Input placeholder='placeholder'/>
+		<Input placeholder='placeholder' disabled/>
+		<div class='wrap'>
+			<Textarea placeholder='placeholder'></Textarea>
+		</div>
+		<div class='wrap'>
+			<Textarea placeholder='placeholder' disabled></Textarea>
+		</div>
+		<div class='wrap'>
+			<Paging></Paging>
+		</div>
+		<Dropdown></Dropdown>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+		}
+	},
+
+	
+	computed: {
+
+	},
+	methods: {
+	
+	},
+}
+</script>
+
+<style lang='scss' scoped>
+	.app{
+		padding: 30px;
+		width: 580px;
+		background-color: $basic-white;
+		margin: 30px auto;
+	}
+
+	.wrap {
+		margin-bottom: 20px;
+	}
+</style>
---
+++ src/components/Select.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class='dropdown-wrapper' :class='additionalClass'>
+		<div class='input-list'>
+			<Checkbox 
+				v-for='item in arr' 
+				:text='item' 
+				:key='arr.indexOf(item)' 
+				:id='item'
+			></Checkbox>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default {
+	data() {
+		return {
+			arr: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8']
+		}
+	},
+
+	props: {
+		'additionalClass': {
+			type: String
+		}
+	},
+}
+</script>
+
+<style lang='scss'>
+
+.dropdown-wrapper {
+	width: calc(100% + 10px);
+	position: absolute;
+	border-radius: 3px;
+	background-color: $basic-white;
+	top: -5px;
+	left: -5px;
+	z-index: 2;
+	display: none;
+
+	&-show {
+		border: 1px solid $primary;
+		box-shadow: 0px 0px 2px 2px rgba($primary, 0.5);
+		padding: 32px 0 2px 0;
+		display: block;
+	}
+}
+
+.input {
+	&-list {
+		max-height: 200px;
+		overflow: auto;
+		@include scroll-style;
+	}
+}
+</style>
