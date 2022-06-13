c26bf12b65c0514d180278816a78c5cef78b712d
unknown <antonenko.a.b@gmail.com>
2022-06-13 12:42:53 +0300

    Небольшой рефактринг

--- src/common/directives/AutoHeight.js
+++ src/common/directives/AutoHeight.js
@@ -5,29 +5,27 @@ const autoHeight = {
 		const padding = 10;
 		const maxHeight = 510;
 		el.style.height = 'auto';
-		el.style.maxHeight = maxHeight + 'px';
+		el.style.maxHeight = `${maxHeight}px`;
 		
 		if (el.scrollHeight < minHeight) {
-			el.style.height = minHeight + 'px';
 			defaultHeight = minHeight;
-		} else if (el.scrollHeight > minHeight && el.scrollHeight > maxHeight){
-			el.style.height = el.scrollHeight + 'px';
+		} else if (el.scrollHeight > minHeight && el.scrollHeight < maxHeight){
 			defaultHeight = el.scrollHeight;
 		} else {
-			el.style.height = maxHeight + 'px';
 			defaultHeight = maxHeight;
 		}
-		
+
+		el.style.height = `${defaultHeight}px`;
+
 		el.addEventListener('input', ()=>{
-			el.style.height = defaultHeight + 'px';
+			el.style.height = `${defaultHeight}px`;
 			if(el.scrollHeight > defaultHeight && el.scrollHeight < maxHeight) {
-				el.style.height = (el.scrollHeight + padding) +'px';
+				el.style.height = `${el.scrollHeight + padding}px`;
 			} else if (el.scrollHeight > maxHeight){
-				el.style.height = maxHeight + 'px';
+				el.style.height = `${maxHeight}px`;
 			}
 		})
-		
-	}
+	},
 }
 
 export default _.cloneDeep(autoHeight);
